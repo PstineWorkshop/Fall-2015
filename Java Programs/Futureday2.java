06b0c2231d494c26c9f19cf79a178ef0ccdd866d
PstineWorkshop <31455515+PstineWorkshop@users.noreply.github.com>
2017-09-07 17:31:20 -0500

    Update Futureday2.java

--- Java Programs/Futureday2.java
+++ Java Programs/Futureday2.java
@@ -1,3 +1,8 @@
+/*
+	Write a Java program that prompts the user for current day in number form and days eslapsed.
+	The program will display what the current day is. It will also display what the day will be after the days elasped.
+	The current day must be between 0 and 6 and the program must check this.
+*/
 import java.util.Scanner;
 
 public class Futureday2 {//start of class
@@ -5,46 +10,31 @@ public class Futureday2 {//start of class
     Scanner keyboard = new Scanner(System.in);
     
     int cd;//current days
-    
-    int de;//days elasped
-    
+ 	int de;//days elasped
     int fd;//future days
-    
     boolean valid=true;
-    
     System.out.print(" enter an integer value that is between 0 to 6 for todays date ");
-    
     cd = keyboard.nextInt();
     System.out.print(" enter the number of days elasped since today ");
-
     de = keyboard.nextInt();
+    System.out.print(" the current day is: ");
+    valid =currentday(cd);
     
-      System.out.print(" the current day is: ");
-     valid =currentday(cd);
-     
-     
-     if(valid==true)
-     {
+    if(valid==true){
      
      fd=(cd+de)%7;
-     
      System.out.print(" the future day is: ");
-     
      currentday(fd); 
-     }
+    }
       
      //futureday(cd,de); //futureday(cd,de);
     
-    }// end of main methpod
-public static boolean currentday(int a)
-{
-
-boolean acceptable = true;
-
-if (a>6 || a<0){
-    
-    System.out.println("not available ");
-    acceptable=false;
+   }// end of main methpod
+  public static boolean currentday(int a){
+	boolean acceptable = true;
+	if (a>6 || a<0){
+     System.out.println("not available ");
+     acceptable=false;
     }
     else if (a == 0){
      System.out.print(" sunday ");
@@ -77,7 +67,7 @@ if (a>6 || a<0){
 
    return acceptable;
 
-}
+  }
 
 
 }//end of class
