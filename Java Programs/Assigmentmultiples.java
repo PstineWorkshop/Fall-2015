984d4770758339824437a2dfdfcd3d3bafaea1c8
PstineWorkshop <31455515+PstineWorkshop@users.noreply.github.com>
2017-09-07 16:59:34 -0500

    Update Assigmentmultiples.java

--- Java Programs/Assigmentmultiples.java
+++ Java Programs/Assigmentmultiples.java
@@ -1,36 +1,30 @@
-public class Assigmentmultiples
-{
-    public static void main(String[] args)
-{
+/*
+ The directions are the print all the numbers between 100 and 200 including 200 that, are either multiples of 6 or 5 but not both.
+ Also there can be no more than 10 numbers per line.
+
+*/
+public class Assigmentmultiples {
+   public static void main(String[] args){
    int i=0;
    
    int count=0;
    
-   for(i = 100; i <=200;i++)
-{
-      if (i%5==0 && i%6!=0)
-      {
-      System.out.print(i + " ");
-      count++;
+   for(i = 100; i <=200;i++){
+      if (i%5==0 && i%6!=0){
+         System.out.print(i + " ");
+         count++;
       }
-      else if( i%6==0 && i%5 !=0)
-      {
-      System.out.print(i + " ");
-      count++;
+      else if( i%6==0 && i%5 !=0){
+        System.out.print(i + " ");
+        count++;
       }
+      if(count == 10){//This is used to make sure that line rule works
+         System.out.println();
+         count=0;
+      }
+   }
    
-
-
-      if(count == 10)
-      {
-      System.out.println();
-      count=0;
-      }
  }
-   
-}
-
-
 }
 /*------------------------------------------------------------------------
  
