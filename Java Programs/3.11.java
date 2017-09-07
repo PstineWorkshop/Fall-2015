aa1d8cdfa5a8760e0439c43d5695d691062aa8b3
PstineWorkshop <31455515+PstineWorkshop@users.noreply.github.com>
2017-09-07 16:54:57 -0500

    Update 3.11.java

--- Java Programs/3.11.java
+++ Java Programs/3.11.java
@@ -1,33 +1,28 @@
 public class Assigmentmultiples
 {
-    public static void main(String[] args)
-{
-   int i=0;
+    public static void main(String[] args){
+        int i=0;
    
-   int count;
+        int count;
    
-   for( int i = 100; i <=200;i++)
-{
-   if (i%5==0)
-   {
-   System.out.print(i + " ");
-   count++;
-   }
-   else if( i%6==0)
-   {
-   System.out.print(i + " ");
-   count++;
-   }
+        for( int i = 100; i <=200;i++){
+            if (i%5==0) {
+                 System.out.print(i + " ");
+                 count++;
+            }
+            else if( i%6==0) {
+                 System.out.print(i + " ");
+                count++;
+            }
    
 
-}
-   if(count ==10)
-   {
-   System.out.println();
-   count=0;
-   }
+        }
+        if(count ==10) {
+            System.out.println();
+            count=0;
+        }
    
-}
+    }
 
 
 }
